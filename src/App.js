bac267e17b9728f39daf56de886c99ffcd68916a
Chris Lackey <clackey@runbox.com>
2020-05-07 15:44:13 -0400

    testing fonts and some bulma customization

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1 class="title">PLAYER STATS</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, ultricies hac mollis consequat leo placerat sociis, auctor congue imperdiet turpis interdum mattis. Leo aliquam inceptos vitae purus tempus ultrices eleifend nunc venenatis turpis viverra, potenti metus egestas volutpat hendrerit augue lacus integer interdum class, montes tortor lacinia rhoncus ut vestibulum accumsan imperdiet placerat eget. Fusce tortor quisque vitae phasellus velit sociosqu orci mollis eu, dis leo erat himenaeos sapien condimentum nec cras, nullam justo accumsan suspendisse odio bibendum natoque sem. Eget ad senectus mus netus mollis dignissim montes imperdiet cum porta, integer cras conubia dapibus nisl hendrerit blandit aliquam.</p>
-      <p>Tristique venenatis in litora penatibus purus facilisi odio potenti sociis ullamcorper eget, semper mattis ridiculus habitant euismod nascetur non quam lectus. Et venenatis cursus nec blandit aliquam pulvinar morbi, risus sagittis lectus molestie accumsan condimentum luctus eleifend, mi mauris nostra ultricies ultrices ante.</p>
+      <div className="section">
+        <div className="container">
+          <h1 className="title has-text-centered is-uppercase">Player Stats</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, ultricies hac mollis consequat leo placerat sociis, auctor congue imperdiet turpis interdum mattis. Leo aliquam inceptos vitae purus tempus ultrices eleifend nunc venenatis turpis viverra, potenti metus egestas volutpat hendrerit augue lacus integer interdum class, montes tortor lacinia rhoncus ut vestibulum accumsan imperdiet placerat eget. Fusce tortor quisque vitae phasellus velit sociosqu orci mollis eu, dis leo erat himenaeos sapien condimentum nec cras, nullam justo accumsan suspendisse odio bibendum natoque sem. Eget ad senectus mus netus mollis dignissim montes imperdiet cum porta, integer cras conubia dapibus nisl hendrerit blandit aliquam.</p>
+          <br />
+          <p>Tristique venenatis in litora penatibus purus facilisi odio potenti sociis ullamcorper eget, semper mattis ridiculus habitant euismod nascetur non quam lectus. Et venenatis cursus nec blandit aliquam pulvinar morbi, risus sagittis lectus molestie accumsan condimentum luctus eleifend, mi mauris nostra ultricies ultrices ante.</p>
+        </div>
+      </div>
     </div>
   );
 }
